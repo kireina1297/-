43e291ce933ade528d5625a81143703d08404958
VOIND <39300267+VOIND@users.noreply.github.com>
2019-01-02 23:37:53 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,14 +11,14 @@ OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus
 def load_opus_lib(opus_libs=OPUS_LIBS):
     if opus.is_loaded():
         return True
- 
+   
     for opus_lib in opus_libs:
             try:
                 opus.load_opus(opus_lib)
                 return
             except OSError:
                 pass
- 
+          
     raise RuntimeError('Could not load an opus lib. Tried %s' % (', '.join(opus_libs)))
  
 load_opus_lib()
