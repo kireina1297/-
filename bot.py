08dd2c43fb3746da5b24b6bcc3452061131362f9
VOIND <39300267+VOIND@users.noreply.github.com>
2019-01-02 23:36:34 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,24 @@
+import discord
+import asyncio
+import os
+from discord import opus
+from discord.ext import commands
+from discord.ext.commands import Bot
+from discord.voice_client import VoiceClient
 
+OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+ 
+def load_opus_lib(opus_libs=OPUS_LIBS):
+    if opus.is_loaded():
+        return True
+ 
+    for opus_lib in opus_libs:
+            try:
+                opus.load_opus(opus_lib)
+                return
+            except OSError:
+                pass
+ 
+    raise RuntimeError('Could not load an opus lib. Tried %s' % (', '.join(opus_libs)))
+ 
+load_opus_lib()
