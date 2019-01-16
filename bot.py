dbfe47cec6790f021924e443535cd6c824862fc5
kireina1297 <46320166+kireina1297@users.noreply.github.com>
2019-01-16 14:10:34 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,7 +37,7 @@ async def on_member_join(member):
 @bot.event
 async def on_message(message):
     chanel - message.channel
-    if message.content.startswith('우유'):
+    if message.content.startswith('우유' ):
         await bot.send._message(chanel, 'milk:')
     
 bot.run(os.environ['TOKEN'])
