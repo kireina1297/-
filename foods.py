7c8d9f51c00e8891e658c48d184243fa6e550c87
VOIND <39300267+VOIND@users.noreply.github.com>
2019-01-31 16:06:43 +0900

    Update foods.py

--- foods.py
+++ foods.py
@@ -1,1 +1,25 @@
+import discord
+from discord.ext import commands
 
+class foods:
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command(pass_context = True)
+    async def 우유(self, ctx):
+        channel = ctx.message.channel
+        await self.bot.send_message(channel, ':milk:')
+        
+    @commands.command(pass_context = True)
+    async def 밥(self, ctx):
+        channel = ctx.message.channel
+        await self.bot.send_message(channel, ':fish::milk:')
+        
+    @commands.command(pass_context = True)
+    async def 녹차(self, ctx):
+        channel = ctx.message.channel
+        await self.bot.send_message(channel, ':tea:')
+        
+def setup(bot):
+    bot.add_cog(foods(bot))
+    print('음식 파일이 준비되었습니다.')
