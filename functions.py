94307e952da2a6c52e7552c8f1cd346f8c8cad30
VOIND <39300267+VOIND@users.noreply.github.com>
2019-01-31 16:41:56 +0900

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
 import discord
+import asyncio
+import time
 from discord.ext import commands
 
 class functions:
@@ -16,8 +18,9 @@ class functions:
     async def 추방시켜(self, ctx, username: discord.User):
         await self.bot.ban(username)
         await self.bot.say('{}(이)가 추방되었습니다.'.format(username))
+        await asyncio.sleep(1)
         await self.bot.say('{}(은)는 더이상 돌아오지 못합니다.'.format(username))
 
 def setup(bot):
     bot.add_cog(functions(bot))
-    print('삭제 파일이 로드 되었습니다.')
+    print('기능 파일이 준비되었습니다.')
