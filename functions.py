9c87268c8f0c63a1a311af04e2e79fd8e1804c0b
VOIND <39300267+VOIND@users.noreply.github.com>
2019-02-01 21:15:12 +0900

    Update functions.py

--- functions.py
+++ functions.py
@@ -9,17 +9,22 @@ class functions:
         
     @commands.command(pass_context = True)
     @commands.has_permissions(kick_members=True)
-    async def 내보내(self, ctx, username: discord.User):
-        await self.bot.kick(username)
-        await self.bot.say('{}(이)가 퇴장되었습니다.'.format(username))
+    async def 내보내(self, ctx, member: discord.Member=None):
+        if not member:
+            await self.bot.say('정확한 사람을 골라야합니다.')
+            return
+        await member.kick()
+        await self.bot.say('{}(이)가 퇴장되었습니다.'.format(member))
         
     @commands.command(pass_context = True)
     @commands.has_permissions(ban_members=True)
-    async def 추방시켜(self, ctx, username: discord.User):
-        await self.bot.ban(username)
-        await self.bot.say('{}(이)가 추방되었습니다.'.format(username))
+    async def 추방시켜(self, ctx, member: discord.Member=None):
+        if not member:
+            await self.bot.say('정확한 사람을 골라야합니다.')
+        await member.ban()
+        await self.bot.say('{}(이)가 추방되었습니다.'.format(member))
         await asyncio.sleep(1)
-        await self.bot.say('{}(은)는 더이상 돌아오지 못합니다.'.format(username))
+        await self.bot.say('{}(은)는 더이상 돌아오지 못합니다.'.format(member))
 
 def setup(bot):
     bot.add_cog(functions(bot))
