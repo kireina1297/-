5b12473b0445e6493ec15d463c2dd52acee8b744
VOIND <39300267+VOIND@users.noreply.github.com>
2019-02-04 21:41:57 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,10 +46,10 @@ async def 도와줘(ctx):
         title = '도움이 필요하십니까?',
         color = 0xffffff
     )
-    hm.add_field(name='우유', value='우유를 가져다 드리겠습니다.', inline=True)
-    hm.add_field(name='밥', value='우유와 생선을 가져다 드리겠습니다.', inline=True)
-    hm.add_field(name='녹차', value='녹차를 끓여오겠습니다.', inline=True)
-    hm.add_field(name='카레', value='카레를 가져다 드리겠습니다.', inline=True)
+    hm.add_field(name='우유:milk:', value='우유를 가져다 드리겠습니다.', inline=True)
+    hm.add_field(name='밥:fish::milk:', value='우유와 생선을 가져다 드리겠습니다.', inline=True)
+    hm.add_field(name='녹차:tea:', value='녹차를 끓여오겠습니다.', inline=True)
+    hm.add_field(name='카레:curry:', value='카레를 가져다 드리겠습니다.', inline=True)
     await bot.send_message(channel, embed=hm)
     
 if __name__ == '__main__':
