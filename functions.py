0e3588c6a4ee773970b20acccfc35c43555ce5e2
kireina1297 <46320166+kireina1297@users.noreply.github.com>
2019-03-28 12:55:18 +0900

    Update functions.py

--- functions.py
+++ functions.py
@@ -11,7 +11,7 @@ class functions:
     @commands.command(pass_context = True)
     async def 쫒아내(self, ctx, member: discord.Member=None):
         if not member:
-            await self.bot.say('정확한 사람을 골라야합니다.')
+            await self.bot.say('존재자체가 없습니다.')
             return
         await self.bot.kick(member)
         await self.bot.say('{}(이)을 쫒아냈습니다.'.format(member))
@@ -20,7 +20,7 @@ class functions:
     @commands.command(pass_context = True)
     async def 죽여(self, ctx, member: discord.Member=None):
         if not member:
-            await self.bot.say('정확한 사람을 골라야합니다.')
+            await self.bot.say('존재자체가 없습니다.')
         await self.bot.ban(member)
         await self.bot.say('{}(이)가 죽었습니다.'.format(member))
         await asyncio.sleep(1)
