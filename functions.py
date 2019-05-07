af0bbc8bc04f51d7e3ab6e8e967217323ac14b00
kireina1297 <46320166+kireina1297@users.noreply.github.com>
2019-05-07 19:11:43 +0900

    Update functions.py

--- functions.py
+++ functions.py
@@ -32,9 +32,9 @@ class functions:
     if ctx.message.author.server_permissions.administrator:
         user = ctx.message.author
         role = discord.utils.get(user.server.roles, name="Muted")
-        await client.add_roles(user, role)
-
-       embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff00f6)
+         await client.add_roles(user, role)
+     else:
+       embed=discord.say(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff00f6)
        await bot.say(embed=embed)
         
 def setup(bot):
