19819274110d57e7c0290d1b616b56c0c9bc807a
kireina1297 <46320166+kireina1297@users.noreply.github.com>
2019-05-07 19:09:59 +0900

    Update functions.py

--- functions.py
+++ functions.py
@@ -28,13 +28,12 @@ class functions:
         await self.bot.say('{}(은)는 더이상 돌아오지 못합니다.'.format(member))
         
 #mute        
-bot.command(pass_context = True)
-async def mute(ctx, user_id, userName: discord.User):
+    async def 입막어(ctx, user_id, userName: discord.User):
     if ctx.message.author.server_permissions.administrator:
         user = ctx.message.author
         role = discord.utils.get(user.server.roles, name="Muted")
         await client.add_roles(user, role)
-     else:
+
        embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff00f6)
        await bot.say(embed=embed)
         
