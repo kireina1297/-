6b66eb9aa5c98a5bd56619821c041515ea9fecfa
kireina1297 <46320166+kireina1297@users.noreply.github.com>
2019-08-16 13:49:32 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ extensions = ['foods', 'functions']
 @bot.event
 async def on_ready():
     #await bot.change_presence(game=discord.Game(name='Yes, My Lord'))#봇 상태메세지
-    game = discord.Game("Yes, My Lord"
+    game = discord.Game("Yes, My Lord")
    print("봇이 준비되었습니다.주인님")
 
 @bot.event
